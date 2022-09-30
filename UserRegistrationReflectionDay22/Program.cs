bef9d4a5df244178f3b25ecb9f11995ac410e17f
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-30 20:56:01 +0530

    [Lokesh]Added:User registration using reflection

---
+++ UserRegistrationReflectionDay22/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationReflectionDay22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ UserRegistrationReflectionDay22/Validation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationReflectionDay22
+{
+    internal class Validation
+    {
+    }
+}
